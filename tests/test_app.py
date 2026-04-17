446014efed1a58e069e18e544510956cb463e10a
KEROJAM <majorekcs@hotmail.com>
2026-04-16 18:15:27 -0600

    tests fix

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'app'))
+
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["message"] == "Hola desde Flask en Docker"
