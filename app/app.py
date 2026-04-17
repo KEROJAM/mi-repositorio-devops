3858db61679ae43e1762dce258f0d7792583199b
KEROJAM <majorekcs@hotmail.com>
2026-04-16 18:22:54 -0600

    fix app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask.jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Hola desde Flask en Docker"})
 
-if __name__ == "__main__"
-    app.run(host="0.0.0.0".port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
